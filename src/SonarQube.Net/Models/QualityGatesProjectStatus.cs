b8cba05dd44e33a97a7ab36a035750f9931d4dc2
Fábio Junqueira <fabioblumm@cwi.com.br>
2020-12-21 14:04:19 -0300

    Updaing the json structure of the GetQualityGatesProjectStatusAsync response

--- src/SonarQube.Net/Models/QualityGatesProjectStatus.cs
+++ src/SonarQube.Net/Models/QualityGatesProjectStatus.cs
@@ -2,11 +2,16 @@
 
 namespace SonarQube.Net.Models
 {
-	public class QualityGatesProjectStatus
-	{
-		public string Status { get; set; }
-		public bool? IgnoredConditions { get; set; }
-		public IEnumerable<QualityGateCondition> Conditions { get; set; }
-		public IEnumerable<QualityGatePeriod> Periods { get; set; }
-	}
+    public class QualityGatesProjectStatus
+    {
+        public QualityGatesProjectStatusData ProjectStatus { get; set; }
+    }
+
+    public class QualityGatesProjectStatusData
+    {
+        public string Status { get; set; }
+        public bool? IgnoredConditions { get; set; }
+        public IEnumerable<QualityGateCondition> Conditions { get; set; }
+        public IEnumerable<QualityGatePeriod> Periods { get; set; }
+    }
 }
